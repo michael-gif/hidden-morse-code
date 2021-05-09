66b70f0eccce564e4fc4890366d4b4c1d86745ec
Bob <mwxfranco@gmail.com>
2021-05-09 14:15:37 +0100

    improved the morse reader, and removed the compact version

--- morse_reader - verbose.py
+++ morse_reader - verbose.py
@@ -4,35 +4,88 @@ im = Image.open(input('filename (including extension): >>'))
 
 pix = im.load()
 
-morse_code = [
-    '. ...',
-    '... . . .',
-    '... . ... .',
-    '... . .',
-    '.',
-    '. . ... .',
-    '... ... .',
-    '. . . .',
-    '. .',
-    '. ... ... ...',
-    '... . ...',
-    '. ... . .',
-    '... ...',
-    '... .',
-    '... ... ...',
-    '. ... ... .',
-    '... ... . ...',
-    '. ... .',
-    '. . .',
-    '...',
-    '. . ...',
-    '. . . ...',
-    '. ... ...',
-    '... . . ...',
-    '... . ... ...',
-    '... ... . .',
-    ' '
-    ]
+morse_code = {
+    'a': '. ...',
+    'b': '... . . .',
+    'c': '... . ... .',
+    'd': '... . .',
+    'e': '.',
+    'f': '. . ... .',
+    'g': '... ... .',
+    'h': '. . . .',
+    'i': '. .',
+    'j': '. ... ... ...',
+    'k': '... . ...',
+    'l': '. ... . .',
+    'm': '... ...',
+    'n': '... .',
+    'o': '... ... ...',
+    'p': '. ... ... .',
+    'q': '... ... . ...',
+    'r': '. ... .',
+    's': '. . .',
+    't': '...',
+    'u': '. . ...',
+    'v': '. . . ...',
+    'w': '. ... ...',
+    'x': '... . . ...',
+    'y': '... . ... ...',
+    'z': '... ... . .',
+    ' ': ' '
+    }
+
+morse_code = {
+    'a': '.-',
+    'b': '-...',
+    'c': '-.-.', 
+    'd': '-..',
+    'e': '.',
+    'f': '..-.',
+    'g': '--.',
+    'h': '....',
+    'i': '..',
+    'j': '.---',
+    'k': '-.-',
+    'l': '.-..',
+    'm': '--',
+    'n': '-.',
+    'o': '---',
+    'p': '.--.',
+    'q': '--.-',
+    'r': '.-.',
+    's': '...',
+    't': '-',
+    'u': '..-',
+    'v': '...-',
+    'w': '.--',
+    'x': '-..-',
+    'y': '-.--',
+    'z': '--..',
+    '0': '-----',
+    '1': '.----',
+    '2': '..---',
+    '3': '...--',
+    '4': '....-',
+    '5': '.....',
+    '6': '-....',
+    '7': '--...',
+    '8': '---..',
+    '9': '----.',
+    '.':'.-.-.-',
+    ', ':'--..--',
+    '?':'..--..',
+    '/':'-..-.',
+    '-':'-....-',
+    '=':'-...-',
+    '(':'-.--.',
+    ')':'-.--.-',
+    '\'':'.----.',
+    '_':'..--.-',
+    '&':'.-...',
+    '"':'.-..-.',
+    ';':'-.-.-.',
+    '$':'...-..-'
+}
 
 def convert(im):
     # take all the pixels and put them into a list
@@ -40,16 +93,26 @@ def convert(im):
     # split the morse up into words, removing any whitespace in the process
     morse_words = [word for word in morse.split('       ') if word.strip() != '' ]
 
+    # generate a new dict of morse code, with the dashes replaced with three dots each
+    morsecode = {}
+    for key, value in morse_code.items():
+        temp = []
+        for char in value:
+            if char == '-':
+                temp.append('...')
+            else:
+                temp.append('.')
+        morsecode[key] = ' '.join(temp)
+        temp = []
+
+    # the morse code dict but reversed
+    morsecode_reversed = {value:key for key,value in morsecode.items()}
+
     # convert morse code into english
-    alphabet = 'abcdefghijklmnopqrstuvwxyz'
     sentence = []
     for word in morse_words:
         letters = word.split('   ')
-        new_word = ''
-        for letter in letters:
-            index = morse_code.index(letter)
-            letter = alphabet[index]
-            new_word += letter
+        new_word = ''.join(morsecode_reversed.get(letter) for letter in letters)
         sentence.append(new_word)
 
     return ' '.join(sentence)
